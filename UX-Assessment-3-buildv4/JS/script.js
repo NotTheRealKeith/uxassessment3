4aa8b5f51f90f764ad45f677f2685f3907effe7c
NotTheRealKeith <keith.baloyo@gmail.com>
2021-04-08 14:05:13 +1200

    Add files via upload

---
+++ UX-Assessment-3-buildv4/JS/script.js
@@ -0,0 +1,9 @@
+console.log('javascriipt');
+
+//used for testing jquery
+// $('.navContainer').hide();
+
+//html document must be ready before Jquery can trigger any functionality
+$(document).ready(function(){
+
+});
